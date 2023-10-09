77bf06752120d867b1e656393786b2a224cf51cf
leeoofeerbf <leeoofeerbf@gmail.com>
2023-10-09 22:04:40 +0200

    Added CropPot system - Missing link it with the selectCrop script

---
+++ Assets/Scripts/CropPot.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+
+public class CropPot : MonoBehaviour
+{
+    public PlantType plantType;
+    private bool isGrowing = false;
+    private float growthTimer = 0f;
+
+    void Update()
+    {
+        // Check if the crop pot is growing a plant
+        if (isGrowing)
+        {
+            growthTimer += Time.deltaTime;
+
+            // Check if the plant has grown completely
+            if (growthTimer >= plantType.growTime)
+            {
+                // Plant is finished growing
+                SetStatus("Finished");
+                isGrowing = false;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Starts growing the plant
+    /// </summary>
+    public void StartGrowingPlant()
+    {
+        isGrowing = true;
+        SetStatus("Growing");
+    }
+
+    /// <summary>
+    /// Collects the plant from the crop pot and adds it to the player's inventory then resets the crop pot
+    /// </summary>
+    public void CollectPlant()
+    {
+        // Check if there is a plant in the crop pot and if it's either growing or finished growing
+        if (plantType != null && !isGrowing )
+        {
+            // Perform collect action
+            // TODO: Add the plant to the player's inventory, play a sound, etc.
+            SetStatus("Collected");
+
+            // Reset the crop pot for the next use
+            ResetCropPot();
+        }
+        else
+        {
+            SetStatus("No plant or plant not ready");
+        }
+    }
+
+    // Helper (for demonstration purposes)
+    private void SetStatus(string status)
+    {
+        Debug.Log($"Crop Pot Status: {status}");
+    }
+
+    private void ResetCropPot()
+    {
+        plantType = null; 
+        growthTimer = 0f;
+        isGrowing = false;
+        SetStatus("Empty");
+    }
+}
+
+
---
+++ Assets/Scripts/Objects Types/PlantType.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[System.Serializable]
+public class PlantType
+{
+    public Sprite plantIcon; 
+    public string plantName; 
+    public float growTime;
+}
